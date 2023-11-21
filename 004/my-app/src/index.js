5b801cbffa54dde0c0762134390c703b8aed6338
Igor <gizmo0787@gmail.com>
2023-11-21 20:33:27 +0300

    005

--- 004/my-app/src/index.js
+++ 004/my-app/src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const text = "Hello world!";
+
+const elem = (
+  <div>
+    <h2 className="header">Текст: {text}</h2>
+    <input type="text" />
+    <button tabIndex='0'>Click</button>
+    <label htmlFor=""></label>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  elem
 );
