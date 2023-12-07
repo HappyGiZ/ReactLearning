8d46f20eba8d30c14a421b39afb366a5cdb71a51
Igor <gizmo0787@gmail.com>
2023-12-07 19:07:28 +0300

    008 done

--- 008 App/my-app/src/components/app/app.js
+++ 008 App/my-app/src/components/app/app.js
@@ -1,7 +1,8 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
-import EmployersListItem from "../employers-list-item/employers-list-item";
+import EmployersList from "../employers-list/employers-list";
+import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
@@ -15,8 +16,8 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployersListItem />
-
+      <EmployersList />
+      <EmployersAddForm />
     </div>
   );
 }
