02670c3316f4f0ed8f4f6d23ff877f5f856dd694
Igor <gizmo0787@gmail.com>
2024-01-07 18:44:16 +0300

    010

--- 008 App/my-app/src/components/app/app.js
+++ 008 App/my-app/src/components/app/app.js
@@ -7,6 +7,14 @@ import EmployersAddForm from "../employers-add-form/employers-add-form";
 import "./app.css";
 
 function App() {
+
+  const data = [
+    { name: 'Ivan Kuzmin', salary: 950, increase: false },
+    { name: 'Kuzma Ivanov', salary: 1900, increase: true },
+    { name: 'Fedor Petrov', salary: 2300, increase: false },
+    { name: 'King Burger', salary: 830, increase: true },
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -16,7 +24,7 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployersList />
+      <EmployersList data={data} />
       <EmployersAddForm />
     </div>
   );
